50fd6374a306c7d6b058a0ac31b691fcc2d651c0
mmetcalf14 <mekena.mcgrew@gmail.com>
2016-01-06 09:06:14 -0800

    Trying to get this onlin

---
+++ TestGSL_Class/GSL_Test.h
@@ -0,0 +1,13 @@
+//
+//  GSL_Test.h
+//  TestGSL_Class
+//
+//  Created by mekena McGrew on 1/5/16.
+//  Copyright © 2016 Mekena Metcalf. All rights reserved.
+//
+
+#ifndef GSL_Test_h
+#define GSL_Test_h
+
+
+#endif /* GSL_Test_h */
---
+++ TestGSL_Class/GetSoln.cpp
@@ -0,0 +1,9 @@
+//
+//  GetSoln.cpp
+//  TestGSL_Class
+//
+//  Created by mekena McGrew on 1/6/16.
+//  Copyright © 2016 Mekena Metcalf. All rights reserved.
+//
+
+#include <stdio.h>
